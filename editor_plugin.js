11ba1fd6d4aeec81c80800ff054be28c15b733a4
愚人码头 <feiwen8772@qq.com>
2017-08-24 14:47:52 +0800

    新增显示文件名及突出显示重点代码行
    2017年08月24日 v3.0 更新：
    
    -
    新增显示文件名，显示文件名称时，不显示语言类型；文本编辑器中可以直接通过添加`filename-`加文件名的方式添加，例如:`filename-
    index.html`。
    - 新增显示重点代码行，可以在`prettify.css`文件的`.important` 中自定义设置重点行样式，多行用英文逗号 “,”
    隔开；文本编辑器中可以直接通过添加`importlines:`加重点行号的方式添加，例如:`importlines:1,3,4`
    ；特别注意：行号从`1` 开始索引，不是`0`;
    - 重构了可视化编辑器插入代码的功能。

--- editor_plugin.js
+++ editor_plugin.js
@@ -13,6 +13,7 @@
 tinymce.PluginManager.add('wpgcp', function (editor, url) {
     var isArray = tinymce.util.Tools.isArray;
     var $ = window.jQuery;
+
     function escapeHtml(text) {
         return text.replace(/&/g, "&amp;").replace(/"/g, "&quot;").replace(/'/g, "'").replace(/</g, "&lt;").replace(/>/g, "&gt;");
     }
@@ -20,44 +21,65 @@ tinymce.PluginManager.add('wpgcp', function (editor, url) {
     function showDialog() {
         var win = editor.windowManager.open({
             title: '嵌入代码',
-            width: 500,
-            height: 440,
+            width: 800,
+            height: 540,
 
             html: ['<form name="wpgcp" action="#" style="padding: 20px">',
-                '    <!--<h3>选择语言</h3>-->',
-                '    <table border="0" cellpadding="4" cellspacing="0">',
-                '        <tr>',
-                '            <td nowrap="nowrap"><label for="wpgcp_main">选择语言</label></td>',
-                '            <td><select id="wpgcp_lang" name="wpgcp_main"  style="width:300px;border: 1px solid' +
-                ' #E1E1E1">',
-                '                    <option value="JavaScript" selected>JavaScript</option>',
-                '                    <option value="ECMAScript6">ECMAScript 6</option>',
-                '                    <option value="CommandLine">CommandLine</option>',
-                '                    <option value="jQuery">jQuery</option>',
-                '                    <option value="CSS">CSS</option>',
-                '                    <option value="Sass">Sass</option>',
-                '                    <option value="HTML">HTML</option>',
-                '                    <option value="package.json">package.json</option>',
-                '                </select></td>',
-                '        </tr>',
-                '    </table>',
                 '    <h3>插入代码：</h3>',
-                '    <div>',
-                '        <textarea name="" style="border: 1px solid #E1E1E1;height:300px; width:440px;" id="wpgcp_code"></textarea>',
+                '    <div><textarea name="" style="border: 1px solid #E1E1E1;height:300px; width:740px;" id="wpgcp_code"></textarea>',
                 '    </div>',
-                '    <table border="0" cellpadding="4" cellspacing="0">',
+                '    <table border="0" cellpadding="4" cellspacing="0"  style="margin-top:15px;">',
+                '        <tbody>',
                 '        <tr>',
-                '            <td nowrap="nowrap" valign="top"><label for="wpgcp_linenumber">开始行</label>',
-                '            </td>',
-                '            <td><input type="text" name="linenumber" id="wpgcp_linenumber" value="1" size="3"/></td>',
+                '            <td nowrap="nowrap" valign="top" style="padding:6px 0">选择语言</td>',
+                '            <td  style="padding:6px 0"><label for="wpgcp_main1"><input type="radio" id="wpgcp_main1" name="wpgcp_lang" checked',
+                '                                                value="JavaScript"/>',
+                '                JavaScript',
+                '            </label>',
+                '                <label for="wpgcp_main2"><input type="radio" id="wpgcp_main2" name="wpgcp_lang"',
+                '                                                value="CSS"/>',
+                '                    CSS',
+                '                </label>',
+                '                <label for="wpgcp_main3"><input type="radio" id="wpgcp_main3" name="wpgcp_lang"',
+                '                                                value="HTML"/>',
+                '                    HTML',
+                '                </label>',
+                '                <label for="wpgcp_main4"><input type="radio" id="wpgcp_main4" name="wpgcp_lang"',
+                '                                                value="CommandLine"/>',
+                '                    CommandLine',
+                '                </label>',
+                '                <label for="wpgcp_main5"><input type="radio" id="wpgcp_main5" name="wpgcp_lang"',
+                '                                                value="Sass"/>',
+                '                    Sass',
+                '                </label>',
+                '                <label for="wpgcp_main6"><input type="radio" id="wpgcp_main6" name="wpgcp_lang"',
+                '                                                value="jQuery"/>',
+                '                    jQuery',
+                '                </label>',
+                '                <label for="wpgcp_main7"><input type="radio" id="wpgcp_main7" name="wpgcp_lang"',
+                '                                                value="ECMAScript 6"/>',
+                '                    ECMAScript 6',
+                '                </label></td>',
                 '        </tr>',
-                '        ',
+                '        <tr>',
+                '            <td nowrap="nowrap" valign="top" style="padding:6px 0"><label for="wpgcp_filename">文件名</label></td>',
+                '            <td style="padding:6px 0"><input type="text" name="filename" id="wpgcp_filename" value=""></td>',
+                '        </tr>',
+                '        <tr>',
+                '            <td nowrap="nowrap" valign="top" style="padding:6px 0"><label for="wpgcp_linenumber">开始行</label></td>',
+                '            <td style="padding:6px 0"><input type="text" name="linenumber" id="wpgcp_linenumber" value="1" size="3"> 开始行号</td>',
+                '        </tr>',
+                '        <tr>',
+                '            <td nowrap="nowrap" valign="top" style="padding:6px 0"><label for="wpgcp_importlines">重点行</label></td>',
+                '            <td style="padding:6px 0"><input type="text" name="importlines" id="wpgcp_importlines" value=""> 从1开始索引，多号以英文逗号 "," 隔开</td>',
+                '        </tr>',
+                '        </tbody>',
                 '    </table>',
                 '</form>'].join(""),
             buttons: [{
                 text: ' 插入代码 ',
                 subtype: 'primary',
-                style:'width:88px',
+                style: 'width:88px',
                 onclick: function () {
                     insertContents()
                 }
@@ -70,13 +92,21 @@ tinymce.PluginManager.add('wpgcp', function (editor, url) {
         });
 
         function insertContents() {
-            var $win=$(win.$el);
+            var $win = $(win.$el);
             var tagtext;
-            var langname = $win.find('#wpgcp_lang').val();
-            var linenumber = $win.find('#wpgcp_linenumber').val();
-
+            var langname = $win.find('input[name=wpgcp_lang]').val();
+            var linenumber = $.trim($win.find('#wpgcp_linenumber').val());
+            var filename = $.trim($win.find('#wpgcp_filename').val());
+            var importlines = $.trim($win.find('#wpgcp_importlines').val());
             var html = escapeHtml($win.find('#wpgcp_code').val());
-            tagtext = '<pre class="prettyprint lang-' + langname + ' linenums:' + linenumber + '" >' + html + '</pre>';
+            tagtext = '<pre class="prettyprint lang-' + langname + ' linenums:' + linenumber;
+            if (!!filename) {
+                tagtext += ' filename-' + filename;
+            }
+            if (!!importlines) {
+                tagtext += ' importlines:' + importlines;
+            }
+            tagtext += '" >' + html + '</pre>';
             editor.insertContent(tagtext);
             win.close();
         }
--- wp-wpgcp-button.php
+++ wp-wpgcp-button.php
@@ -3,7 +3,7 @@
 Plugin Name:WP-googlecodeprettify
 Plugin URI: http://www.css88.com/archives/4804
 Description: 一个基于 Google Code Prettify 实现的WordPress代码高亮插件，在TinyMCE编辑器中提供了一个插入代码的按钮。在文本编辑器中，加入了pre-js,pre-css,pre-html 3个快捷按钮;
-Version: 2.0
+Version: 3.0
 Author: 愚人码头
 Author URI:  http://www.css88.com/
 */
